3ad94704dbb39d762a19010bea8c0e847c73cd03
mshum <michael.shum@moesol.com>
2019-06-06 18:29:58 -0700

    space

--- cachefun/cache.py
+++ cachefun/cache.py
@@ -1,17 +1,22 @@
 import hashlib
 import xxhash
-
+import os
 class Cache:
 
   def __init__(self):
     self.files = {}
+    fsStats = os.statvfs()
+    self.freeSpace = fsStats.f_bsize*fsStats.f_bavail
 
   def write(self, data):
+    if self.freeSpace < len(data):
+      print("NOT ENOUGH SPACE!!!")
     #hasher = hashlib.sha256()
     hasher = xxhash.xxh64()
     hasher.update(data)
     filename = hasher.hexdigest()
     self.files[filename] = data
+    self.freeSpace -= len(data)
     return filename
 
   def read(self, hashStr):
