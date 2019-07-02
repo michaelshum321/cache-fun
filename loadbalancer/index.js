8c22bb07dd3c368bd1aa816a921d99c952877d28
Michael Shum <poopyfeet@casual-printer.local>
2019-07-01 22:08:40 -0700

    fix some tiny typos

--- loadbalancer/index.js
+++ loadbalancer/index.js
@@ -4,42 +4,48 @@ const xxhash = require('xxhash')
 const app = express()
 const port = process.env.PORT || 8090
 const cacheUrl = process.env.CACHE_URL || 'http://localhost:8080'
-
+const seed = 0x13371337
 const files = {}
 
 // TODO: handle not in cache
 app.get('/get', (req, res) => {
   const id = req.query.id
   if (!id) {
-    res.status(400).end({error:'no id given'})
+    res.status(400).json({error:'no id given'})
+    return
   }
 
   // check if exists
-  if (!(id in hashedFiles)) {
-    res.status(404).send()
+  if (!(id in files)) {
+    res.status(404).end()
+    return
   }
   // if exists, fetch from cache
   axios.get(cacheUrl+'/get/'+id, {timeout:10000})
     .then((response) => {
       // TODO parse err in cache response ?
       res.send(response.data)
+      return
     })
     .catch((err) => {
-      res.status(400).send({error:'cache request failed'})
-      console.error(err)
+      console.log(err)
+      res.status(400).json({error:'cache request failed'})
+      return
     })
 })
 
 app.post('/save',(req,res) => {
   // get file
   const data = req.body
+  console.log(req.body)
   if (!data) {
-    res.send({error: 'no data posted'})
+    res.status(400).json({error: 'no data posted'})
+    return
   }
   // hash
-  const hasher = new xxhash();
   const buf = Buffer.from(data)
-  const hash = hasher.hash64(buf)
+  const hash = xxhash.hash64(buf,seed,'hex')
+  console.log('hash',hash)
   if (hash in files) {
     res.send({id:hash})
     return
@@ -52,9 +58,10 @@ app.post('/save',(req,res) => {
     })
     .catch((err) => {
       console.log(err);
-      res.status(400).send({error:'cache request failed'})
+      res.status(400).json({error:'cache request failed'})
     })
 })
 
+app.use(express.json())
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
